0ebfba9aefeb8aca2c7ae0a531ca35c0581778ad
mete5050 <33295066+mete5050@users.noreply.github.com>
2019-08-17 11:23:13 +0300

    Create arayuz.py

---
+++ arayuz.py
@@ -0,0 +1,40 @@
+import pyserial
+ard = serial.Serial('/dev/ttyUSB0')
+#ard.write()
+print(psari , "Islem Seciniz: ")
+
+normal = '\033[0m'
+kirmizi= '\033[31m'
+yesil= '\033[32m'
+sari= '\033[33m'
+lacivert= '\033[34m'
+mor= '\033[35m'
+mavi= '\033[36m'
+pmavi = '\033[96m'#p --> parlak
+pkirmizi= '\033[91m'
+pyesil = '\033[92m'
+psari = '\033[93m'
+siyah = '\033[90m'
+asiyah= '\033[40m'#a --> arkaplan
+akirmizi= '\033[41m'
+ayesil= '\033[42m'
+asari= '\033[43m'
+alacivert= '\033[44m'
+amor= '\033[45m'
+amavi= '\033[46m'
+abeyaz= '\033[47m'
+apsiyah= '\033[100m'#a --> arkaplan-parlak
+apkirmizi= '\033[101m'
+apyesil= '\033[102m'
+apsari= '\033[103m'
+aplacivert= '\033[104m'
+apmor= '\033[105m'
+apmavi= '\033[106m'
+apbeyaz= '\033[107m'
+
+
+
+
+
+
+
