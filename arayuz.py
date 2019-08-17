3578b953578b3e617e4d0c6bad881c0dbcf15f2e
mete5050 <33295066+mete5050@users.noreply.github.com>
2019-08-17 13:35:27 +0300

    Update arayuz.py

--- arayuz.py
+++ arayuz.py
@@ -33,24 +33,64 @@ apbeyaz= '\033[107m'
 
 
 
-import pyserial
+import serial
 ard = serial.Serial('/dev/ttyUSB0')
 #ard.write()
+
+#menu1
 print(psari , "Islem Seciniz: ")
 print(pmavi ," 1 - PWM-Servo/ESC ureteci")
+print(pmavi ,"2 - PWM ureteci(Duty Cycle)")
+print(pmavi ,"3 - Dalga jeneratorü")
+print(pmavi ,"4 - PWM ölçer")
+
+menu1 = input()
+
+if (menu1 == 1):
+	#menu2
+	print(pyesil ,"1 - Manual")
+	print(pyesil ,"2 - Sweep")
+	print(pyesil ,"3 - ESC Başlatıcı")
+	if(menu2 == 1):
+		#frekans = input("Frekansı gitriniz (Hz): ")
+		sure = input("Acik kalma suresini giriniz(800-2500) : ")
+		ard.write(1)
+		ard.write(sure)
+
+	if(menu2 == 2):
+		ard.write(2)
+		aah == input("Artıs-Azalıs hızını girin (Milisecond) : ")
+	
+	if(menu2 == 3):
+		ard.write(3)
+
+if(menu1 == 2):
+	duty = input("Duty Cycle giriniz (%) : ")
+	ard.write(4)
+	ard.wrtite(dc)
+        frekans = input("Frekansı gitriniz (Hz): ")
+	ard.write(frekans)
 
 
+if(menu1 == 3):
+	#menu3
+	print("Dalga türünü secin : ")
+	print(pyesil ,"1 - Kare")
+	print(pyesil ,"2 - Sinus")
+	print(pyesil ,"3 - Ucgen")	
+	menu3 = input()
+	frekans = input("Frekansı gitriniz (Hz): ")
 
+	if(menu3 == 1):
+		ard.write()
 
+	if(menu3 == 2):	
+		ard.write()
 
+	if(menu3 == 3):	
+		ard.write()
 
-
-
-
-
-
-
-
+	ard.write(frekans)
 
 
 
